31f88e45eb16924bbde4227cc659cb7c02ae8064
LED-0102 <darshilamit@gmail.com>
2024-03-20 11:34:16 +0530

    bug fixes

--- src/static/login.js
+++ src/static/login.js
@@ -3,24 +3,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("my-form");
 
     form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        const formData = {
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value,
-        };
+        // event.preventDefault();
+        // const formData = {
+        //     username: document.getElementById("username").value,
+        //     password: document.getElementById("password").value,
+        // };
 
-        console.log(formData);
+        // console.log(formData);
 
-        const xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "https://aptabase.shuttleapp.rs/v1/auth/login");
-        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhttp.send(
-            JSON.stringify({
-                email: formData.username,
-                password: formData.password,
+        // const xhttp = new XMLHttpRequest();
+        // xhttp.open("POST", "https://aptabase.shuttleapp.rs/v1/auth/login");
+        // xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        // xhttp.send(
+        //     JSON.stringify({
+        //         email: formData.username,
+        //         password: formData.password,
+        //     })
+        // );
+        //
+        // console.log(xhttp.responseText);
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent the default form submission behavior
+
+            // Get form data
+            const formData = {
+                username: document.getElementById("username").value,
+                password: document.getElementById("password").value,
+            };
+
+            // Make the API call
+            fetch('https://aptabase.shuttleapp.rs/v1/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
             })
-        );
-
-        console.log(xhttp.responseText);
+                .then(response => {
+                    console.log(response);
+                    if (response.ok) {
+                        prompt("Data submitted successfully");
+                    } else if (response.status === 400) {
+                        prompt("Bad Request. Please try again");
+                    }
+                });
+        });
     });
 });
