f349ab6b83e783e02a8aa5aad3fc6ddbf16a285d
LED-0102 <darshilamit@gmail.com>
2024-03-20 10:52:30 +0530

    bug fixes

---
+++ src/static/database.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tableBody = document.getElementById('table-body');
+    const authToken = localStorage.getItem('authtoken');
+
+    // Redirect to admin login page if auth token is not present
+    if (!authToken) {
+        window.location.href = '/static/admin.html';
+    } else {
+        fetchAndPopulateData(); // Call function to fetch and populate data
+    }
+
+    function fetchAndPopulateData() {
+        fetch(`https://aptabase.shuttleapp.rs/v1/fetch`, {
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                tableBody.innerHTML = ''; // Clear existing table rows
+
+                // Populate table with fetched data
+                data.forEach(item => {
+                    const newRow = document.createElement('tr');
+                    newRow.innerHTML = `
+                  <td>${item.aptamer}</td>
+                  <td>${item.target}</td>
+                  <td>${item.apt_type}</td>
+                  <td>${item.length}</td>
+                  <td>${item.sequence}</td>
+                  <td>
+                      <button class="reject-btn" onclick="handleAction('${item.id}')">Delete</button>
+                  </td>
+              `;
+                    newRow.querySelector('.reject-btn').addEventListener('click', () => handleAction(item.id));
+                    tableBody.appendChild(newRow);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
+
+    // Function to handle action button click
+    function handleAction(itemId) {
+        fetch(`https://aptabase.shuttleapp.rs/v1/admin/delete/${itemId}`, { // Fixed typo here from delele to delete
+            method: 'DELETE'
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data); // Handle response data if needed
+            })
+            .catch(error => {
+                console.error('Error performing action:', error);
+            });
+    }
+});
---
+++ src/static/login.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("my-form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const formData = {
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value,
+        };
+
+        console.log(formData);
+
+        const xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "https://aptabase.shuttleapp.rs/auth/login");
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(
+            JSON.stringify({
+                email: formData.username,
+                password: formData.password,
+            })
+        );
+
+        console.log(xhttp.responseText);
+    });
+});
