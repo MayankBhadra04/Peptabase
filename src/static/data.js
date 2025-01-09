2d1af7fc952b1a22a2d0135b373beb51f411162a
tanmaisai1208 <tanmaisaich@gmail.com>
2025-01-09 15:36:02 +0530

    Create data.js

---
+++ src/static/data.js
@@ -0,0 +1,35 @@
+const resources = [
+    {
+      type: "image",
+      url: "https://via.placeholder.com/100", 
+      description: "Placeholder Image 1",
+    },
+    {
+      type: "image",
+      url: "https://via.placeholder.com/200",
+      description: "Placeholder Image 2",
+    },
+    {
+      type: "image",
+      url: "https://via.placeholder.com/200",
+      description: "Placeholder Image 3",
+    },
+    {
+      type: "pnb",
+      url: "https://example.com/dummy.pnb", 
+      description: "Dummy PNB File 1",
+    },
+    {
+      type: "pnb",
+      url: "https://example.com/sample.pnb",
+      description: "Dummy PNB File 2",
+    },
+    {
+      type: "pnb",
+      url: "https://example.com/dummy.pnb", 
+      description: "Dummy PNB File 3",
+    },
+  ];
+  
+  export default resources;
+  
